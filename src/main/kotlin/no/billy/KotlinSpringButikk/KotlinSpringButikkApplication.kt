137b79987c0a6b1b113d5d5fd0372c0a5cfd673d
Drefeng <infbil15@gmail.com>
2019-07-24 18:08:25 +0200

    initial stage

---
+++ src/main/kotlin/no/billy/KotlinSpringButikk/KotlinSpringButikkApplication.kt
@@ -0,0 +1,11 @@
+package no.billy.KotlinSpringButikk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringButikkApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringButikkApplication>(*args)
+}
